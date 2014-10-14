cd7c4992d5d52cd09f33c2fadaeb1a7444aacf94
Nigger Niggersson <baot@ewq.se>
2014-10-14 20:00:15 +0200

    added test files added file as argument

--- test/Main.hs
+++ test/Main.hs
@@ -6,6 +6,9 @@ module Main where
 import Parser
 
 import Control.Monad.Trans
+
+import System.IO
+import System.Environment
 import System.Console.Haskeline
 
 process :: String -> IO ()
@@ -15,11 +18,55 @@ process line = do
     Left err -> print err
     Right ex -> mapM_ print ex
 
-main :: IO ()
-main = runInputT defaultSettings loop
+repl :: IO ()
+repl = runInputT defaultSettings loop
   where
   loop = do
     minput <- getInputLine "ready> "
     case minput of
       Nothing -> outputStrLn "Goodbye."
       Just input -> (liftIO $ process input) >> loop
+
+
+processFile :: String -> IO ()
+processFile fname = readFile fname >>= process
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    []      -> repl
+    [fname] -> processFile fname >> return ()
+
+--import qualified LLVM.General.AST as AST
+
+--initModule :: AST.Module
+--initModule = emptyModule "my cool jit"
+
+{-
+process :: AST.Module -> String -> IO (Maybe AST.Module)
+process modo source = do
+  let res = parseToplevel source
+  case res of
+    Left err -> print err >> return Nothing
+    Right ex -> do
+      ast <- codegen modo ex
+      return $ Just ast
+
+processFile :: String -> IO (Maybe AST.Module)
+processFile fname = readFile fname >>= process initModule
+
+repl :: IO ()
+repl = runInputT defaultSettings (loop initModule)
+  where
+  loop :: AST.Module -> InputT IO ()
+  loop mod = do
+    minput <- getInputLine "ready> "
+    case minput of
+      Nothing -> outputStrLn "Goodbye."
+      Just input -> do
+        modn <- lift $ process mod input
+        case modn of
+          Just modn -> loop modn
+          Nothing -> loop mod
+-}
