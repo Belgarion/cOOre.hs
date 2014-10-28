4398f93f64a373f1c6941c6f88fe9a9cbffae536
Sebastian Larsson <belgarion@brokenbrain.se>
2014-10-28 15:46:09 +0100

    Better handling of tyes

--- TypeCheck.hs
+++ TypeCheck.hs
@@ -10,25 +10,33 @@ typecheck [] _ = []
 typecheck ((Klass name expr):ast) env = typecheck (expr ++ ast) env
 typecheck ((Function t name params stmts):ast) env = typecheck (params ++ stmts ++ ast) env -- checka även params mot typer, lägg in funktionstyp i env
 typecheck ((Var name):ast) env = typecheck (ast) env
-    where newenv = Data.Map.insert name "type" env
 typecheck ((BinaryOp name left right):ast) env =
     (x):(typecheck (left:(right:ast)) newenv) -- lägg in i env vid =
     where
         (x, newenv) = case (name) of
             "=" -> case (left, right) of
-                ((Var name),t) -> ("= OK " ++ name, Data.Map.insert name (typetostring t) env)
+                ((Var name),t) -> ("= OK " ++ name, Data.Map.insert name (typetostring t env) env)
                 otherwise -> ("= ERROR (" ++ (show left) ++ ") " ++ name ++ " ("++ (show right) ++ ")", env)
             "+" -> case (left, right) of
-                ((Int _),(Int _)) -> ("+ OK", env)
-                ((Float _),(Float _)) -> ("+ OK", env)
-                ((String _),(String _)) -> (" + OK", env)
-                ((Var name),t) -> if ts==ts2 then ("+ OK", env) else ("+ ERROR: Unmatched types: (" ++ ts2 ++ ") (" ++ ts ++ ")", env)
+                (t,t2) -> if ts==ts2 then ("+ OK", env) else ("+ ERROR: Unmatched types: (" ++ ts ++ ") (" ++ ts2 ++ ")", env)
                     where
-                    ts = typetostring t
-                    ts2 = case (Data.Map.lookup (name) env) of
-                        Nothing -> "undeclared variable " ++ name
-                        Just s -> s
-                otherwise -> ("+ ERROR: Unmatched types: (" ++ (show left) ++ ") (" ++ (show right) ++ ")", env)
+                    ts = typetostring t env
+                    ts2 = typetostring t2 env
+            "-" -> case (left, right) of
+                (t,t2) -> if ts==ts2 then ("+ OK", env) else ("+ ERROR: Unmatched types: (" ++ ts ++ ") (" ++ ts2 ++ ")", env)
+                    where
+                    ts = typetostring t env
+                    ts2 = typetostring t2 env
+            "*" -> case (left, right) of
+                (t,t2) -> if ts==ts2 then ("+ OK", env) else ("+ ERROR: Unmatched types: (" ++ ts ++ ") (" ++ ts2 ++ ")", env)
+                    where
+                    ts = typetostring t env
+                    ts2 = typetostring t2 env
+            "/" -> case (left, right) of
+                (t,t2) -> if ts==ts2 then ("+ OK", env) else ("+ ERROR: Unmatched types: (" ++ ts ++ ") (" ++ ts2 ++ ")", env)
+                    where
+                    ts = typetostring t env
+                    ts2 = typetostring t2 env
             otherwise -> ("Unhandled binaryop " ++ name, env)
 typecheck ((Call name params):ast) env = typecheck (params ++ ast) env
 typecheck ((Float value):ast) env = typecheck (ast) env
@@ -42,9 +50,12 @@ typecheck ((Return expr):ast) env = typecheck (expr:ast) env
 typecheck ((Claim name stmts):ast) env = typecheck (stmts ++ ast) env
 typecheck (expr:ast) env = ("Other " ++ (show expr)):(typecheck ast env)
 
-typetostring :: Expr -> String
-typetostring (String _) = "string"
-typetostring (Int _) = "int"
-typetostring (Float _) = "float"
-typetostring (Void) = "void"
-typetostring x = "unknown (" ++ (show x) ++ ")"
+typetostring :: Expr -> Data.Map.Map String String -> String
+typetostring (String _) _ = "string"
+typetostring (Int _) _ = "int"
+typetostring (Float _) _ = "float"
+typetostring (Void) _ = "void"
+typetostring (Var name) env = case (Data.Map.lookup (name) env) of
+                        Nothing -> "undeclared variable " ++ name
+                        Just s -> s
+typetostring x _ = "unknown (" ++ (show x) ++ ")"
